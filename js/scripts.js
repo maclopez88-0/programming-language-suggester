494e4560efca1dd23100ce85a11b40d673387601
Cesar Lopez <lopez.cesar.aug@gmail.com>
2021-06-19 16:24:29 -0700

    add hand dominance question

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,17 @@
 $(document).ready(function() {
   $("form#programs").submit(function(event) {
-    const yourName = $("input#yourName").val();
+    const name = $("input#name").val();
     const season = $("input#season").val();
     const astrologySign = $("input#astrologySign").val();
+    const randomNumber = parseInt($("input#randomNumber").val());
+    const hand = $("select#hand").val();
 
 
-    
-  })
-})
+    if (season === "Fall" || "Summer" || "Winter" && astrologySign === "Cancer") {
+      alert("Based on your answers, we have calculated that Python is your best match.");
+    } else {
+      alert("Ruby is calling your name")
+    }
+    event.preventDefault();
+  });
+});
