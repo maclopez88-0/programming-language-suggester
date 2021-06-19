4b00a18e06b2ca5efc4a5e222cb342be7cae46a8
Cesar Lopez <lopez.cesar.aug@gmail.com>
2021-06-18 23:24:19 -0700

    replace inputs with descriptive names. WIP

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $("form#programs").submit(function(event) {
+    const yourName = $("input#yourName").val();
+    const season = $("input#season").val();
+    const astrologySign = $("input#astrologySign").val();
+
+
+    
+  })
+})
