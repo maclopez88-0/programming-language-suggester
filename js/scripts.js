49e43297b99cf5f7ad698b3d6e074b2bca11d0a7
Cesar Lopez <lopez.cesar.aug@gmail.com>
2021-06-19 18:51:29 -0700

    renamed index_2 to index

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,19 @@
 $(document).ready(function() {
   $("form#programs").submit(function(event) {
+    event.preventDefault();
     const name = $("input#name").val();
     const season = $("#season").val();
     const avatar = $("#avatar").val();
     const hand = $("select#hand").val();
 
-    // if (season === "Fall" || "Summer" || "Winter" && avatar === "Fire Nation") {
-    //   alert(", based on your answers, we have calculated that Python is your best match.");
-    // } else {
-    //   alert("Ruby is calling your name")
-    // }
-
-
     if (season === "Fall" || "Summer" || "fall" && avatar === "Air Nomads" || "Water Tribe" || "Fire Nation" || "Earth Kingdom" && hand === "Left handed") {
-      alert("You may want to take up Python!")
+      alert("You may want to take up Python!");
     }
-    else if (season === "Fall" || "Summer" || "fall" && avatar === "Air Nomads" || "Water Tribe" || "Fire Nation" || "Earth Kingdom" && hand === "Right handed" {
-      alert("You may want to look into C#")
+    else if (season === "Fall" || "Summer" || "fall" && avatar === "Air Nomads" || "Water Tribe" || "Fire Nation" || "Earth Kingdom" && hand === "Right handed") {
+      alert("You may want to look into C#");
     }
-    else (season === "spring" && avatar === "Air Nomads" || "Water Tribe" || "Fire Nation" || "Earth Kingdom" && hand === "Right handed" || "Left handed") (
-      alert("According to our calculations, Ruby is right for you!")
-    )
-    event.preventDefault();
+    else (season === "spring" && avatar === "Air Nomads" || "Water Tribe" || "Fire Nation" || "Earth Kingdom" && hand === "Right handed" || "Left handed") {
+      alert("According to our calculations, Ruby is right for you!");
+    }
   });
 });
