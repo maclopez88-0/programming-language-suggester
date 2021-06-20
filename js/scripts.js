55e38dada8a16906daffcbf3854f7934bda32ad1
Cesar Lopez <lopez.cesar.aug@gmail.com>
2021-06-19 18:16:56 -0700

    add if else to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,26 @@
 $(document).ready(function() {
   $("form#programs").submit(function(event) {
     const name = $("input#name").val();
-    const season = $("input#season").val();
-    const astrologySign = $("input#astrologySign").val();
-    const randomNumber = parseInt($("input#randomNumber").val());
+    const season = $("#season").val();
+    const avatar = $("#avatar").val();
     const hand = $("select#hand").val();
 
+    // if (season === "Fall" || "Summer" || "Winter" && avatar === "Fire Nation") {
+    //   alert(", based on your answers, we have calculated that Python is your best match.");
+    // } else {
+    //   alert("Ruby is calling your name")
+    // }
 
-    if (season === "Fall" || "Summer" || "Winter" && astrologySign === "Cancer") {
-      alert("Based on your answers, we have calculated that Python is your best match.");
-    } else {
-      alert("Ruby is calling your name")
+
+    if (season === "Fall" || "Summer" || "fall" && avatar === "Air Nomads" || "Water Tribe" || "Fire Nation" || "Earth Kingdom" && hand === "Left handed") {
+      alert("You may want to take up Python!")
     }
+    else if (season === "Fall" || "Summer" || "fall" && avatar === "Air Nomads" || "Water Tribe" || "Fire Nation" || "Earth Kingdom" && hand === "Right handed" {
+      alert("You may want to look into C#")
+    }
+    else (season === "spring" && avatar === "Air Nomads" || "Water Tribe" || "Fire Nation" || "Earth Kingdom" && hand === "Right handed" || "Left handed") (
+      alert("According to our calculations, Ruby is right for you!")
+    )
     event.preventDefault();
   });
 });
